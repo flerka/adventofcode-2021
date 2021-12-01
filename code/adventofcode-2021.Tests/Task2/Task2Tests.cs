35922b0309df92f1cfdcd08535e8a991bcc548e7
Marya <marya.kozhemyakina@gmail.com>
2021-12-01 15:34:48 +0200

    Day 1 Task 2

---
+++ code/adventofcode-2021.Tests/Task2/Task2Tests.cs
@@ -0,0 +1,44 @@
+using Xunit;
+using adventofcode_2021.Task2;
+using System.Collections.Generic;
+using System.IO;
+
+namespace adventofcode_2021.Tests
+{
+    public class Task2Tests
+    {
+        [Fact]
+        public void Task2_IfEmpty_Correct()
+        {
+            Assert.Equal(0, Solution.Function(new List<int>()));
+        }
+
+        [Fact]
+        public void Task2_IfOneElement_Correct()
+        {
+            Assert.Equal(0, Solution.Function(new List<int>{ 55 }));
+        }
+
+        [Fact]
+        public void Task2_IfSeveralElements_Correct()
+        {
+            Assert.Equal(5, Solution.Function(
+                new List<int> { 199, 200, 208, 210, 200, 207, 240, 269, 260, 263 }));
+        }
+
+        [Fact]
+        public void Task2_RealExample_Correct()
+        {
+            Assert.Equal(5, Solution.Function(ReadFileAsync(Path.Combine("Task2", "Data.txt"))));
+        }
+
+        private IEnumerable<int> ReadFileAsync(string fileName)
+        {
+            foreach (var line in File.ReadLines(fileName))
+            {
+                yield return int.Parse(line);
+            }
+        }
+
+    }
+}
--- code/adventofcode-2021/Task2/Task2.cs
+++ code/adventofcode-2021/Task2/Task2.cs
@@ -1,16 +1,16 @@
 ﻿using System.Collections.Generic;
 using System.Linq;
 
-namespace adventofcode_2021.Task1
+namespace adventofcode_2021.Task2
 {
     public class Solution
     {
         /// <summary>
-        /// Solution for the first https://adventofcode.com/2021/day/1/ task
+        /// Solution for the second https://adventofcode.com/2021/day/1/ task
         /// </summary>
         public static int Function(IEnumerable<int> input)
         {
-            return input.Skip(1).Zip(input, (curr, prev) => curr > prev ? 1 : 0).Sum();
+            return input.Skip(3).Zip(input, (curr, prev) => curr > prev ? 1 : 0).Sum();
         }
     }
 }
