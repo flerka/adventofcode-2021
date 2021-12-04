913a18e19fd771ce827136a8342032afc0f3f397
Marya <marya.kozhemyakina@gmail.com>
2021-12-04 16:01:41 +0200

    add day 4 tasks

---
+++ code/adventofcode-2021.Tests/Task8/Task8Tests.cs
@@ -0,0 +1,57 @@
+using Xunit;
+using adventofcode_2021.Task8;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace adventofcode_2021.Tests
+{
+    public class Task8Tests
+    {
+        [Fact]
+        public void Task8_RealExample_Correct()
+        {
+            var (numbers, boards, boardsDict) = ReadFileAsync(Path.Combine("Task8", "Data.txt"));
+            Assert.Equal(1924, Solution.Function(numbers, boards, boardsDict));
+        }
+
+        private (List<int>, List<int[,]>, List<Dictionary<int, (int, int)>>) ReadFileAsync(string fileName)
+        {
+            var lines = File.ReadAllLines(fileName);
+            var bingoNumbers = lines.First().Split(',').Select(int.Parse).ToList();
+            var bingoBoards = new List<int[,]>();
+            var bingoBoardsDict = new List<Dictionary<int, (int, int)>>();
+
+            var skipLines = 2; 
+            var bingoSize = 5;
+
+            while (skipLines < lines.Length)
+            {
+                var bingoItem = new int[5, 5];
+                Dictionary<int, (int, int)> bingoItemDict = new();
+
+                lines
+                .Skip(skipLines)
+                .Take(bingoSize)
+                .Aggregate(0, (si, nexts) =>
+                {
+                    nexts.Split(' ', System.StringSplitOptions.RemoveEmptyEntries)
+                     .Aggregate(0, (ni, nextn) =>
+                     {
+                         var val = int.Parse(nextn);
+                         bingoItem[si, ni] = val;
+                         bingoItemDict[val] = (si, ni);
+                         return ni + 1;
+                     });
+                    return si + 1;
+                });
+                
+                bingoBoards.Add(bingoItem);
+                bingoBoardsDict.Add(bingoItemDict);
+                skipLines += (bingoSize + 1);
+            }
+
+            return (bingoNumbers, bingoBoards, bingoBoardsDict);
+        }
+    }
+}
---
+++ code/adventofcode-2021/Task7/Task7.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace adventofcode_2021.Task7
+{
+    public class Solution
+    {
+        /// <summary>
+        /// Solution for the first https://adventofcode.com/2021/day/4/ task
+        /// </summary>
+        public static int Function(List<int> numbers, List<int[,]> boards, List<Dictionary<int, (int, int)>> boardsDict)
+        {
+            var resultCheck = boards;
+            var isWinnerDetected = false;
+            List<int> winnerValue = new();
+
+            for (var i = 0; i < numbers.Count && !isWinnerDetected; i++)
+            {
+                var num = numbers[i];
+
+                boardsDict.Aggregate(0, (r, next) => {
+                    if (next.ContainsKey(num))
+                    {
+                        var (i, j) = next[num];
+                        resultCheck[r][i, j] = int.MaxValue;
+
+                        if (IsWinnerBoard(resultCheck[r], i, j))
+                        {
+                            isWinnerDetected = true;
+                            winnerValue.Add(GetWinValue(resultCheck[r], num));
+                        }
+                    }
+                    return r+1;
+                });
+            }
+
+            return winnerValue.First();
+        }
+
+        private static int GetWinValue(int[,] board, int winnerNumber)
+        {
+            var result = 0;
+            for (var i = 0; i < 5; i++)
+            {
+                for (var j = 0; j < 5; j++)
+                {
+                    if (board[i,j] != int.MaxValue)
+                    {
+                        result += board[i, j];
+                    }
+                } 
+            }
+
+            return result * winnerNumber;
+        }
+        private static bool IsWinnerBoard(int[,] board, int iw, int jw)
+        {
+            // check horizontally
+            var isWinnerHorizontally = true;
+            for (var i = 0; i < 5; i++)
+            {
+                if (board[iw, i] != int.MaxValue)
+                {
+                    isWinnerHorizontally = false;
+                }
+            }
+
+            var isWinnerVertically = true;
+            for (var i = 0; i < 5; i++)
+            {
+                if (board[i, jw] != int.MaxValue)
+                {
+                    isWinnerVertically = false;
+                }
+            }
+
+            return isWinnerHorizontally || isWinnerVertically;
+        }
+    }
+}
