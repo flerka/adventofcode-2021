4b1ccde343f4481814ac5954094552cabb1afa12
flerka <marya.kozhemyakina@gmail.com>
2022-01-17 19:20:43 +0200

    add day 23 solution

--- code/adventofcode-2021/Task45/Task45.cs
+++ code/adventofcode-2021/Task45/Task45.cs
@@ -1,19 +1,302 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 namespace adventofcode_2021.Task45
 {
     public class Solution
     {
-        private class Room
+        public struct Burrow
         {
-            public Room(char top, char bottom, int index, char roomName) { Top = top; Bottom = bottom; Index = index; RoomName = roomName; }
-            public char Top { get; set; }
-            public char Bottom { get; set; }
-            public int Index { get; set; }
-            public char RoomName { get; set; }
+            public List<char> Hall { get; set; }
+            public List<List<char>> Rooms { get; set; } = new();
+        }
+
+        private struct Move
+        {
+            public double TotalCost { get; set; }
+
+            public Burrow StateAfterMove { get; set; }
+        }
+
+
+        private static double solve(Burrow data)
+        {
+            if (done(data.Rooms))
+            {
+                return 0;
+
+            }
+
+            // todo add cache
+            //res, found:= cache[data]
+
+            //if found {
+            //            return res
+            //}
+
+            var best = double.PositiveInfinity;
+
+
+            var moves = getValidMoves(data);
+
+            foreach (var move in moves)
+            {
+                var cost = move.TotalCost;
+                var result = solve(move.StateAfterMove);
+
+                // todo add cache
+                // cache[move.StateAfterMove] = result
+
+                cost += result;
+
+
+                if (cost < best)
+                {
+                    best = cost;
+
+                }
+            }
+
+            return best;
+        }
+
+        private static double getMoveCost(int hall, int room, int depth, Burrow burrow, char r)
+        {
+            int start = 0;
+            int end = 0;
+            var moveRight = true;
+
+            if (hall < 2 * (room + 1))
+            {
+                start = hall + 1;
+                end = 2 * (room + 1);
+            }
+            else
+            {
+                moveRight = false;
+                start = 2 * (room + 1);
+                end = hall - 1;
+            }
+
+
+            foreach (var rr in burrow.Hall.ToArray()[start..(end + 1)])
+            {
+                // hall is blocked
+                if (rr != EmptyChar)
+                {
+                    return double.PositiveInfinity;
+
+                }
+            }
+
+            if (moveRight)
+            {
+                start--;
+
+            }
+            else
+            {
+                end++;
+
+            }
+
+            return (end - start + (depth + 1)) * AmphipodsEnergy[r];
+        }
+
+        private static List<Move> getValidMovesFromHall(Burrow data)
+        {
+            try
+            {
+                List<Move> movesFromHall = new();
+
+                for (int i = 0; i < data.Hall.Count; i++)
+                {
+                    if (data.Hall[i] == EmptyChar)
+                    {
+                        continue;
+                    }
+
+                    var pod = data.Hall[i];
+                    var roomNum = RoomByName(pod);
+                    var room = data.Rooms[roomNum];
+
+                    var depth = RoomSize - 1;
+                    while (depth >= 0)
+                    {
+                        if (depth > 1 || depth < 0)
+                        {
+                            var a = 0;
+                        }
+                        if (room[depth] == pod)
+                        {
+                            depth--;
+                            continue;
+                        }
+
+                        if (room[depth] == EmptyChar)
+                        {
+                            break;
+                        }
+
+                        depth += -1;
+                    }
+
+                    // cannot move if other types of amphipod present in room
+                    if (depth == -1)
+                    {
+                        continue;
+                    }
+
+                    var cost = getMoveCost(i, roomNum, depth, data, pod);
+
+                    // no path or no place in room
+
+                    if (double.IsPositiveInfinity(cost))
+                    {
+                        continue;
+
+                    }
+
+                    var clone = new Burrow
+                    {
+                        Hall = data.Hall.Select(i => i).ToList(),
+                        Rooms = data.Rooms.Select(i => i.Select(k => k).ToList()).ToList()
+                    };
+
+                    clone.Hall[i] = EmptyChar;
+                    clone.Rooms[roomNum][depth] = pod;
+
+
+                    movesFromHall.Add(new Move { TotalCost = cost, StateAfterMove = clone });
+                }
+
+                return movesFromHall;
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
+        }
+
+        private static List<Move> getValidMovesFromRoom(Burrow data)
+        {
+            List<Move> movesFromRoom = new();
+
+            for (var i = 0; i < data.Rooms.Count; i++)
+            {
+                var room = data.Rooms[i];
+                var j = 0;
+
+                while (j < RoomSize)
+                {
+                    if (room[j] == EmptyChar)
+                    {
+                        j++;
+                        continue;
+                    }
+
+                    break;
+                }
+
+                // room is empty
+                if (j == RoomSize)
+                {
+                    continue;
+                }
+
+                var nbr = NameByRoom(i);
+                var containsOnlyOwners = true;
+                for (var z = j; z < RoomSize; z++)
+                {
+                    if (room[z] != nbr)
+                    {
+                        containsOnlyOwners = false;
+                    }
+                }
+
+                if (containsOnlyOwners)
+                {
+                    continue;
+                }
+
+                for (int h = 0; h < data.Hall.Count; h ++)
+                {
+                    if (!EnterableHallCell(h) || data.Hall[h] != EmptyChar)
+                    {
+                        continue;
+                    }
+
+                    var cost = getMoveCost(h, i, j, data, room[j]);
+
+                    // no path or no place in cell
+                    if (double.IsPositiveInfinity(cost))
+                    {
+                        continue;
+                    }
+
+                    // todo clone
+                    var clone = new Burrow
+                    {
+                        Hall = data.Hall.Select(i => i).ToList(),
+                        Rooms = data.Rooms.Select(i => i.Select(k => k).ToList()).ToList()
+                    }; 
+
+                    clone.Hall[h] = clone.Rooms[i][j];
+                    clone.Rooms[i][j] = EmptyChar;
+
+                    movesFromRoom.Add(new Move
+                    {
+                        TotalCost = cost,
+                        StateAfterMove = clone,
+                    });
+                }
+            }
+
+            return movesFromRoom;
+        }
+
+        private static List<Move> getValidMoves(Burrow data)
+        {
+            var movesFromHall = getValidMovesFromHall(data);
+
+            var movesFromRoom = getValidMovesFromRoom(data);
+            movesFromHall.AddRange(movesFromRoom);
+
+            return movesFromHall;
+        }
+
+
+        private static int RoomByName(char name)
+        {
+            return (int)(name - 'A');
+        }
+
+        private static char NameByRoom(int room)
+        {
+            return (char)('A' + (char)room);
+        }
+
+        private static bool done(List<List<char>> rooms)
+        {
+            var c = 'A';
+            foreach (var room in rooms)
+            {
+
+                foreach (var r in room)
+                {
+                    if (r != c)
+                    {
+                        return false;
+
+                    }
+                }
+
+                c++;
+            }
+
+
+            return true;
         }
 
         private static readonly Dictionary<char, int> AmphipodsEnergy = new Dictionary<char, int>
@@ -21,193 +304,31 @@ namespace adventofcode_2021.Task45
            {'A', 1 }, {'B', 10}, {'C', 100}, {'D', 1000}
         };
 
+        private static bool EnterableHallCell(int i) => !(i % 2 == 0 && i != 0 && i != HallSize - 1);
+
+        private static int RoomSize = 2;
         private const char EmptyChar = '.';
-
-        private static readonly List<int> CanStopIndex = new()
-        {
-            0,
-            1,
-            3,
-            5,
-            7,
-            9,
-            10
-        };
-
-        private static readonly List<Room> input = new()
-        {
-            new Room('B', 'A', 2, 'A'),
-            new Room('C', 'D', 4, 'B'),
-            new Room('B', 'C', 6, 'C'),
-            new Room('D', 'A', 8, 'D')
-        };
+        private static int HallSize = 11;
 
         /// <summary>
         /// Solution for the first https://adventofcode.com/2021/day/23/ task.
         /// </summary>
         public static int Function()
         {
-            Dictionary<int, char> corridor = new()
+            var b = new Burrow
             {
-                { 0, EmptyChar },
-                { 1, EmptyChar },
-                { 2, EmptyChar },
-                { 3, EmptyChar },
-                { 4, EmptyChar },
-                { 5, EmptyChar },
-                { 6, EmptyChar },
-                { 7, EmptyChar },
-                { 8, EmptyChar },
-                { 9, EmptyChar },
-                { 10, EmptyChar }
+                Hall = Enumerable.Range(0, 11).Select(_ => EmptyChar).ToList(),
+                Rooms = new List<List<char>> {
+                    new List<char> { 'B', 'A' },
+                    new List<char> { 'C', 'D' },
+                    new List<char> { 'B', 'C' },
+                    new List<char> { 'D', 'A' }
+                }
             };
 
-            var results = new List<int>();
-
-            var d = new Dictionary<int, char>(corridor);
-            var inp = new List<Room>(input);
-
-            var res = Solve(d, inp);
-            if (res != -1)
-            {
-                results.Add(res);
-            }
-
-
-            return 0;
+            var result = solve(b);
+            return Convert.ToInt32(result);
         }
 
-        private static void StateToString(Dictionary<int, char> corridor, List<Room> rooms)
-        {
-            Console.WriteLine("#############");
-            Console.WriteLine($"#{string.Join(string.Empty, corridor.Values.ToList())}#");
-            Console.WriteLine($"###{rooms[0].Top}#{rooms[1].Top}#{rooms[2].Top}#{rooms[3].Top}###");
-            Console.WriteLine($"  #{rooms[0].Bottom}#{rooms[1].Bottom}#{rooms[2].Bottom}#{rooms[3].Bottom}#");
-            Console.WriteLine("  #########");
-        }
-
-        private static int Solve(Dictionary<int, char> corridor, List<Room> rooms)
-        {
-            StateToString(corridor, rooms);
-            for (var roomIndex = 0; roomIndex < rooms.Count; roomIndex++)
-            {
-                if ((rooms[roomIndex].RoomName == rooms[roomIndex].Top) ||
-                    (rooms[roomIndex].Top == EmptyChar && rooms[roomIndex].RoomName == rooms[roomIndex].Bottom))
-                {
-                    continue;
-                }
-
-
-                for (var i = 0; i < corridor.Count; i++)
-                {
-                    if (CanStopIndex.Contains(i) && CanMoveToCorridor(corridor, rooms[roomIndex].Index, i))
-                    {
-                        MoveToCorridor(corridor, rooms, roomIndex, i);
-                        foreach (var key in corridor.Keys)
-                        {
-                            if (CanMoveFromCorridorToRoom(corridor, rooms, key))
-                            {
-                                MoveToTheRoom(corridor, rooms, key);
-                            }
-                        }
-
-                        var a = Solve(new Dictionary<int, char>(corridor), new List<Room>(rooms));
-                    }
-
-                }
-            }
-            return -1;
-        }
-
-        private static void MoveToCorridor(Dictionary<int, char> corridor, List<Room> rooms, int roomI, int dest)
-        {
-            var room = rooms[roomI];
-            if (room.Top != EmptyChar)
-            {
-                corridor[dest] = rooms[roomI].Top;
-                rooms[roomI].Top = EmptyChar;
-            }
-            else
-            {
-                corridor[dest] = rooms[roomI].Bottom;
-                rooms[roomI].Bottom = EmptyChar;
-            }
-        }
-
-        private static void MoveToTheRoom(Dictionary<int, char> corridor, List<Room> rooms, int corridorInd)
-        {
-            var item = corridor[corridorInd];
-            var room = rooms.First(r => r.RoomName == item);
-            if (room.Bottom == EmptyChar)
-            {
-                room.Bottom = item;
-            }
-            else
-            {
-                room.Top = item;
-            }
-            corridor[corridorInd] = EmptyChar;
-        }
-
-        private static bool CanMoveFromCorridorToRoom(Dictionary<int, char> corridor, List<Room> rooms, int corridorInd)
-        {
-
-            var val = corridor[corridorInd];
-            if (val == EmptyChar)
-            {
-                return false;
-            }
-
-            var roomForItem = rooms.FirstOrDefault(item => item.RoomName == val && item.Top == EmptyChar && (item.Bottom == EmptyChar || item.Bottom == val));
-            if (roomForItem == null)
-            {
-                return false;
-            }
-
-            // check if you can go to this room;
-            if (CanMoveToCorridor(corridor, corridorInd, roomForItem.Index))
-            {
-                return false;
-            }
-
-            return true;
-        }
-
-        private static bool CanMoveToCorridor(Dictionary<int, char> corridor, int start, int stop)
-        {
-            if (corridor[stop] != EmptyChar)
-            {
-                return false;
-            }
-
-            if (stop < 0 || stop > 10)
-            {
-                return false;
-            }
-
-            if (stop > start)
-            {
-                for (var i = start + 1; i <= stop; i++)
-                {
-                    if (corridor[i] != EmptyChar)
-                    {
-                        return false;
-                    }
-                }
-
-            }
-            else
-            {
-                for (var i = start - 1; i >= stop; i--)
-                {
-                    if (corridor[i] != EmptyChar)
-                    {
-                        return false;
-                    }
-                }
-            }
-
-            return true;
-        }
     }
 }
